8371b3f4f904322cbc27a93d8fbe2257b1361fd3
Theodoraldo <theodoraldo@gmail.com>
2024-01-25 12:56:17 +0000

    Add history controller for delete, update, get and create actions

---
+++ app/controllers/api/v1/borrow_histories_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::BorrowHistoriesController < ApplicationController
+    before_action :set_history, only: [:show, :update, :destroy]
+
+    
+    def index
+        @histories = BorrowHistory.all
+        render json: @histories
+    end
+
+    def show
+        render json: @history
+    end
+
+    def create
+        @history = BorrowHistory.new(history_params)
+
+        if @history.save
+        render json: @history, status: :created
+        else
+        render json: @history.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @history.update(history_params)
+        render json: @history
+        else
+        render json: @history.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @history.destroy
+        head :no_content
+    end
+
+  
+    private
+  
+    def set_history
+      @history = BorrowHistory.find(params[:id])
+    end
+
+    
+    def history_params
+        params.require(:borrow_history).permit(:borrow_date, :returned_date, :book_state, :instore, :comment, :library_patron_id, :book_id)
+    end
+end
